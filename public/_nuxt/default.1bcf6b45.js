db42c2ea31e8db5af19c81b912215d9cc44e0894
sendoabll <sendoa.barciela@aplazame.com>
2023-03-13 12:16:46 +0100

    Updates

---
+++ public/_nuxt/default.1bcf6b45.js
@@ -0,0 +1,1 @@
+import{a as e}from"./entry.43685732.js";const t={};function r(a,c){return" Default template "}const f=e(t,[["render",r]]);export{f as default};
---
+++ public/_nuxt/index.778d2170.js
@@ -0,0 +1,1 @@
+import{a as e}from"./entry.43685732.js";const r={};function t(c,n){return" Default page "}const _=e(r,[["render",t]]);export{_ as default};
